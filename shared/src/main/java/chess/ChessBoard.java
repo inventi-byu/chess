cdc3dc733d6b72383abac569e5d24245f775ea1c
inventi-byu <bcgunn@byu.edu>
2025-01-28 14:00:49 -0700

    Created new ChessBoard implementation

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,10 +8,11 @@ import java.util.*;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    public ArrayList<ArrayList<ChessPiece>> board;
 
     public ChessBoard() {
-        ArrayList<ArrayList<ChessPiece>> board = new ArrayList<ArrayList<ChessPiece>>(8);
-
+        ArrayList<ArrayList<ChessPiece>> board;
+        this.resetBoard();
         }
 
     /**
@@ -21,7 +22,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        ( board.get(position.getRow()-1) ).add(position.getColumn()-1, piece);
     }
 
     /**
@@ -32,7 +33,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board.get(position.getRow()-1).get(position.getColumn()-1);
     }
 
     /**
@@ -40,6 +41,49 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        this.board = new ArrayList<ArrayList<ChessPiece>>(8);
+        for (int i = 0; i < 8; i++){
+            board.add(i, new ArrayList<ChessPiece>(8));
+        }
+        for (int i = 0; i < 8; i++){
+            switch (i){
+                case 0:
+                    this.addPiece(new ChessPosition(i, 0), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+                    this.addPiece(new ChessPosition(i, 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+                    this.addPiece(new ChessPosition(i, 2), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+                    this.addPiece(new ChessPosition(i, 3), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+                    this.addPiece(new ChessPosition(i, 4), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+                    this.addPiece(new ChessPosition(i, 5), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+                    this.addPiece(new ChessPosition(i, 6), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+                    this.addPiece(new ChessPosition(i, 7), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+                    break;
+                case 1:
+                    // WPR
+                    for (int j = 1; j < 8; j++){
+                        this.addPiece(new ChessPosition(i, j), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+                    }
+                    break;
+                case 6:
+                    // BPR
+                    for (int j = 1; j < 8; j++){
+                        this.addPiece(new ChessPosition(i, j), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+                    }
+                    break;
+                case 7:
+                    // BSR
+                    this.addPiece(new ChessPosition(i, 0), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+                    this.addPiece(new ChessPosition(i, 1), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+                    this.addPiece(new ChessPosition(i, 2), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+                    this.addPiece(new ChessPosition(i, 3), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
+                    this.addPiece(new ChessPosition(i, 4), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
+                    this.addPiece(new ChessPosition(i, 5), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+                    this.addPiece(new ChessPosition(i, 6), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+                    this.addPiece(new ChessPosition(i, 7), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+                    break;
+                default:
+                    break;
+            }
+        }
+
     }
 }
