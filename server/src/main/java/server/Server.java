21bdac9569e8ecf619cf9b37bdf71831edb28622
inventi-byu <bcgunn@byu.edu>
2025-02-27 16:53:17 -0700

    Added port() and empty methods on Server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -22,4 +22,30 @@ public class Server {
         Spark.stop();
         Spark.awaitStop();
     }
+
+    public int port(){
+        return Spark.port();
+    }
+
+    public Object registerUser(){
+        throw RuntimeException("Server.registerUser() is not implemented!");
+    }
+    public Object loginUser(){
+        throw RuntimeException("Server.loginUser() is not implemented!");
+    }
+    public Object logoutUser(){
+        throw RuntimeException("Server.logoutUser() is not implemented!");
+    }
+    public Object listGames(){
+        throw RuntimeException("Server.listGames() is not implemented!");
+    }
+    public Object createGame(){
+        throw RuntimeException("Server.createGame() is not implemented!");
+    }
+    public Object joinGame(){
+        throw RuntimeException("Server.joinGame() is not implemented!");
+    }
+    public Object clear(){
+        throw RuntimeException("Server.clear() is not implemented!");
+    }
 }
