f4bd29c1b78b240820ed8d7ab17d14d53ff5ce53
inventi-byu <bcgunn@byu.edu>
2025-02-27 17:41:13 -0700

    Implemented getAuth()

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,46 @@
 package dataaccess;
 
+import model.AuthData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class MemoryAuthDAO implements AuthDAO {
+
+    Map<String, String> authDB;
+
+    public MemoryAuthDAO(){
+        this.authDB = new HashMap<>();
+    }
+
+    /**
+     * Retrives the AuthData associated with the authToken
+     * @param authToken the token associated with the AuthData
+     * @return the AuthData associated with the token
+     */
+    public AuthData getAuth(String authToken){
+        String username = authDB.get(authToken);
+        if (username != null){
+            return new AuthData(authToken, username);
+        }
+        return null;
+    };
+    
+
+    /**
+     * Adds an AuthData to the database.
+     * @param authData the AuthData to add
+     * @return true if the AuthData was added to the database
+     */
+    public boolean createAuth(AuthData authData){};
+
+    /**
+     * Removes an AuthData from the database
+     * @param authData the AuthData to remove
+     * @return true if the AuthData was successfully removed
+     */
+    public boolean deleteAuth(AuthData authData);
+
+
 }
