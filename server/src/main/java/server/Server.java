75b7bcfeecf4fc7df8de650f35a16dc7c2057620
inventi-byu <bcgunn@byu.edu>
2025-03-04 14:51:59 -0700

    Cleaned up comments on methods in Server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -87,7 +87,6 @@ public class Server {
         return Spark.port();
     }
 
-
     private void exceptionHandler(ResponseException exception, Request req, Response res) {
         res.status(exception.getStatusCode());
         res.body(exception.toJson());
@@ -95,9 +94,9 @@ public class Server {
 
     /**
      * Sends the register request information from the client to the handler.
-     * @param req the http register request from the client
+     * @param req the http register request from the client.
      * @param res the http register response that the server should give.
-     * @return Not sure yet
+     * @return JSON string of the RegisterUserResult body.
      */
     public Object registerUser(Request req, Response res) throws ResponseException {
         return this.UserHandler.handleRegister(req, res);
@@ -105,55 +104,60 @@ public class Server {
 
     /**
      * Sends the login request information from the client to the handler.
-     * @param req the http login request from the client
-     * @param res the http login response that the server should give.
-     * @return Not sure yet
+     * @param req the http login request from the client.
+     * @param res the http login response that the server should give
+     * @return JSON string of the LoginUserResult body.
      */
     public Object loginUser(Request req, Response res){
         return this.SessionHandler.handleLogin(req, res);
         //throw new RuntimeException("Server.loginUser() is not implemented!");
     }
+
     /**
      * Sends the logout request from the client to the handler.
-     * @param req the http logout request from the client
+     * @param req the http logout request from the client.
      * @param res the http logout response that the server should give.
-     * @return Not sure yet
+     * @return JSON string of the LogoutUserResult body.
      */
     public Object logoutUser(Request req, Response res){
         return this.SessionHandler.handleLogout(req, res);
     }
+
     /**
      * Sends the list games request from the client to the handler.
-     * @param req the http list games request from the client
+     * @param req the http list games request from the client.
      * @param res the http list games response that the server should give.
-     * @return Not sure yet
+     * @return JSON string of the ListGamesResult body.
      */
     public Object listGames(Request req, Response res){
         return this.GameHandler.handleListGames(req, res);
     }
+
     /**
      * Sends the create game request from the client to the handler.
      * @param req the http create game request from the client
      * @param res the http create game response that the server should give.
-     * @return Not sure yet
+     * @return JSON string of the CreateGameResult body.
      */
     public Object createGame(Request req, Response res){
         return this.GameHandler.handleCreateGame(req, res);
     }
+
     /**
      * Sends the http join request from the client to the handler.
      * @param req the http join game request from the client
      * @param res the http join game response that the server should give.
-     * @return Not sure yet
+     * @return JSON string of the JoinGameResult body.
      */
     public Object joinGame(Request req, Response res){
         return this.GameHandler.handleJoinGame(req, res);
     }
+
     /**
      * Sends the http clear request from the client to the handler.
      * @param req the http clear request from the client
      * @param res the http clear response that the server should give.
-     * @return Not sure yet
+     * @return JSON string of the ClearResult body.
      */
     public Object clear(Request req, Response res) throws ResponseException {
         return this.ClearHandler.handle(req, res);
