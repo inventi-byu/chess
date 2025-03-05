60fbba7e83e00f18265687900bdc4c2658a0e555
inventi-byu <bcgunn@byu.edu>
2025-03-04 18:10:08 -0700

    Finished writing empty tests

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,9 +1,36 @@
 package service;
 
-public class UserServiceTests {
-}
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-/*
-testRegisterGoodInput
-testRegisterBadInput
- */
+public class UserServiceTests {
+
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        MemoryAuthDAO authDAO = new MemoryAuthDAO();
+        MemoryUserDAO userDAO = new MemoryUserDAO();
+        MemoryGameDAO gameDAO = new MemoryGameDAO();
+        this.userService = new UserService(authDAO, userDAO, gameDAO);
+    }
+
+    @Test
+    @DisplayName("Valid Register Request")
+    public void testRegisterGoodInput() {
+
+
+    }
+
+    @Test
+    @DisplayName("Invalid Logout Request")
+    public void testRegisterBadInput() {
+
+
+    }
+
+}
