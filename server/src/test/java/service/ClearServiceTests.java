eb5c0c05a964b260312cc5963ed8739faeff018e
inventi-byu <bcgunn@byu.edu>
2025-03-04 17:19:38 -0700

    Got IDE to recgonize tests;

--- server/src/test/java/service/ClearServiceTests.java
+++ server/src/test/java/service/ClearServiceTests.java
@@ -1,11 +1,14 @@
-package java.service;
+package service;
 import org.junit.jupiter.api.*;
+import service.ClearService;
+import service.request.ClearRequest;
+import service.result.ClearResult;
 
 public class ClearServiceTests {
     @Test
     public void testClearDatabase(){
-
-        //ClearService clearservice = new ClearService();
+        Assertions.assertTrue(true);
+        // ClearService clearservice = new ClearService();
         //ClearResult result = clearservice.clear(new ClearRequest("delete"));
         //Assertions.assertEquals(new ClearResult(200, ""), result);
     }
