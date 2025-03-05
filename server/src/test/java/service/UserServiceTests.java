200126e6d5007373f1d910becf2bc63cba744e9a
inventi-byu <bcgunn@byu.edu>
2025-03-04 18:25:39 -0700

    Passed userservice tests:

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -3,9 +3,15 @@ package service;
 import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryGameDAO;
 import dataaccess.MemoryUserDAO;
+import model.UserData;
+import org.eclipse.jetty.server.Authentication;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import service.exception.ResponseException;
+import service.request.RegisterRequest;
+import service.result.RegisterResult;
 
 public class UserServiceTests {
 
@@ -22,14 +28,37 @@ public class UserServiceTests {
     @Test
     @DisplayName("Valid Register Request")
     public void testRegisterGoodInput() {
+        String username = "bobLikesChees";
+        String password = "Wisconsin100";
+        String email = "bob@bobisthebomb.com";
+        UserData goodUser = new UserData(username, password, email);
 
+        RegisterRequest request = new RegisterRequest(goodUser);
+        RegisterResult result = this.userService.register(request);
+
+        int expectedStatus = 200;
+        Assertions.assertEquals(expectedStatus, result.getStatus());
+        Assertions.assertEquals(username, result.getAuthData().username());
+        Assertions.assertEquals(String.class, result.getAuthData().authToken().getClass());
 
     }
 
     @Test
-    @DisplayName("Invalid Logout Request")
+    @DisplayName("Invalid Register Request")
     public void testRegisterBadInput() {
 
+        String username = "forgetfulJoe";
+        String password = null;
+        String email = "joe@aol.com";
+        UserData goodUser = new UserData(username, password, email);
+
+        int expectedStatusCode = 400;
+        String expectedMessage = "Error: bad request";
+
+        RegisterRequest request = new RegisterRequest(goodUser);
+        ResponseException exception = Assertions.assertThrows(ResponseException.class, () -> this.userService.register(request));
+        Assertions.assertEquals(expectedStatusCode, exception.getStatusCode());
+        Assertions.assertEquals(expectedMessage, exception.getMessage());
 
     }
 
