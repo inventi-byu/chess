3ec0ddc40a7e37e5cc3237f0c7800641f2c8e4f0
inventi-byu <bcgunn@byu.edu>
2025-03-13 13:10:14 -0600

    Added new empty tests

--- server/src/test/java/dataaccess/AuthDAOTests.java
+++ server/src/test/java/dataaccess/AuthDAOTests.java
@@ -1,4 +1,36 @@
 package dataaccess;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class AuthDAOTests {
+
+    @BeforeEach
+    void setup(){
+        throw new RuntimeException("Not implemented.");
+    }
+
+    public void createAuthTestGoodInput(){
+        throw new RuntimeException("Not implemented.");
+    }
+    public void createAuthTestBadInput(){
+        // User doesn't exist
+        throw new RuntimeException("Not implemented.");
+    }
+
+    public void getAuthTestGoodInput(){
+        throw new RuntimeException("Not implemented.");
+    }
+
+    public void getAuthTestBadInput(){
+        // Authdata doesn't exist
+        throw new RuntimeException("Not implemented.");
+    }
+
+    public void deleteAuthTestGoodInput(){
+        throw new RuntimeException("Not implemented.");
+    }
+
+    public void deleteAuthTestBadInput(){
+        throw new RuntimeException("Not implemented.");
+    }
 }
