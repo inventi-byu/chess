745531a293195457371395e51e90159347f3942c
inventi-byu <bcgunn@byu.edu>
2025-03-13 16:13:48 -0600

    Added testing syntax in GameDAOTests

--- server/src/test/java/dataaccess/GameDAOTests.java
+++ server/src/test/java/dataaccess/GameDAOTests.java
@@ -1,46 +1,56 @@
 package dataaccess;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class GameDAOTests {
 
     @BeforeEach
     void setup(){
-
+        throw new RuntimeException("Not implemented.");
     }
 
+    @Test
     public void addGameTestGoodInput(){
         throw new RuntimeException("Not implemented.");
     }
 
+    @Test
     public void addGameTestBadInput(){
         throw new RuntimeException("Not implemented.");
     }
 
+    @Test
     public void getGameTestGoodInput(){
         throw new RuntimeException("Not implemented.");
     }
 
+    @Test
     public void getGameTestBadInput(){
         throw new RuntimeException("Not implemented.");
     }
 
+    @Test
     public void getAllGamesTestGoodInput(){
         throw new RuntimeException("Not implemented.");
     }
 
+    @Test
     public void getAllGamesTestBadInput(){
         throw new RuntimeException("Not implemented.");
     }
 
+    @Test
     public void addUserToGameTestGoodInput(){
         throw new RuntimeException("Not implemented.");
     }
 
+    @Test
     public void addUserToGameTestBadInput(){
         throw new RuntimeException("Not implemented.");
     }
 
+    @Test
     public void getNewIDTest(){
         // May be handled by the database automatically
         throw new RuntimeException("Not implemented.");
