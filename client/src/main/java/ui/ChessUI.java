ed25999a9ae06dfb1299254e5f2ef67df3dfce64
inventi-byu <bcgunn@byu.edu>
2025-03-17 19:19:28 -0600

    Added ServerFacade and ChessUI classes

---
+++ client/src/main/java/ui/ChessUI.java
@@ -0,0 +1,7 @@
+package ui;
+
+public class ChessUI {
+    public ChessUI (ServerFacade serverFacade){
+        throw new RuntimeException("Not implemented.");
+    }
+}
---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,61 @@
+package ui;
+
+public class ServerFacade {
+    public ServerFacade(){
+        throw new RuntimeException("Not implemented.");
+    }
+
+    /**
+     * Accesses the clear endpoint.
+     * @return boolean if the clear was successful, false if it was not.
+     */
+    public boolean clear(){
+        throw new RuntimeException("Not implemented.");
+    }
+
+    /**
+     * Accesses the register endpoint.
+     * @return boolean if the clear was successful, false if it was not.
+     */
+    public boolean register(String username, String password, String email){
+        throw new RuntimeException("Not implemented.");
+    }
+    /**
+     * Accesses the clear endpoint.
+     * @return boolean if the clear was successful, false if it was not.
+     */
+    public boolean login(String username, String password){
+        throw new RuntimeException("Not implemented.");
+    }
+    /**
+     * Accesses the clear endpoint.
+     * @return boolean if the clear was successful, false if it was not.
+     */
+    public boolean logout(String username){
+        throw new RuntimeException("Not implemented.");
+    }
+
+    /**
+     * Accesses the clear endpoint.
+     * @return boolean if the clear was successful, false if it was not.
+     */
+    public boolean listGames(){
+        throw new RuntimeException("Not implemented.");
+    }
+
+    /**
+     * Accesses the clear endpoint.
+     * @return boolean if the clear was successful, false if it was not.
+     */
+    public boolean createGame(){
+        throw new RuntimeException("Not implemented.");
+    }
+
+    /**
+     * Accesses the clear endpoint.
+     * @return boolean if the clear was successful, false if it was not.
+     */
+    public boolean joinGame(String playerColor, String gameID){
+        throw new RuntimeException("Not implemented.");
+    }
+}
