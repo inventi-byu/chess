3ff294b76c68ef1e2b504805f171a9368b9b6553
inventi-byu <bcgunn@byu.edu>
2025-03-28 20:38:54 -0600

    Still errors in highlight logic

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -490,26 +490,32 @@ public class ChessClient {
         int row = 0;
         int col = 0;
         try {
-            row = Integer.parseInt(
-                    String.valueOf(
-                            positionAsArray[1]
-                    )
-            );
+            // Convert letter to number
+            switch (positionAsArray[0]){
+                case 'a' -> col = 1;
+                case 'b' -> col = 2;
+                case 'c' -> col = 3;
+                case 'd' -> col = 4;
+                case 'e' -> col = 5;
+                case 'f' -> col = 6;
+                case 'g' -> col = 7;
+                case 'h' -> col = 8;
+            }
+            switch (positionAsArray[1]){
+                case '1' -> row = 1;
+                case '2' -> row = 2;
+                case '3' -> row = 3;
+                case '4' -> row = 4;
+                case '5' -> row = 5;
+                case '6' -> row = 6;
+                case '7' -> row = 7;
+                case '8' -> row = 8;
+                case '9' -> row = 9;
+            }
         } catch (Exception exception) {
             return "Sorry, \"" + chessStylePosition + "\" is not valid position.";
         }
 
-        // Convert letter to number
-        switch (positionAsArray[0]){
-            case 'a' -> col = 1;
-            case 'b' -> col = 2;
-            case 'c' -> col = 3;
-            case 'd' -> col = 4;
-            case 'e' -> col = 5;
-            case 'f' -> col = 6;
-            case 'g' -> col = 7;
-            case 'h' -> col = 8;
-        }
         this.setHighlightPosition(new ChessPosition(row, col));
         return "highlight";
     }
