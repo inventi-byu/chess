cb69174868c2fd4aa6419a94a53d61a710add7f2
inventi-byu <bcgunn@byu.edu>
2025-03-31 16:30:11 -0600

    Finished most of the implementation except for NotificationHandler on the ChessUI side

--- client/src/main/java/ui/ChessUI.java
+++ client/src/main/java/ui/ChessUI.java
@@ -180,6 +180,8 @@ public class ChessUI {
                             case 3 -> this.println("You resigned! It\'s all in the mind...");
                             case 4 -> this.println("You resigned! Well that was unexpected.");
                         }
+                        this.displayPostLoginMenu();
+                        this.displayHelpPostLogin();
                         break;
                         
                     case "highlight":
