d08f83831aacc993cbfef5a54d2398ac5c97cb6d
inventi-byu <bcgunn@byu.edu>
2025-03-31 17:34:57 -0600

    Implemented MakeMoveCommand

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -1,4 +1,11 @@
 package websocket.commands;
 
+import chess.ChessMove;
+
 public class MakeMoveCommand extends UserGameCommand {
+    ChessMove move;
+    public MakeMoveCommand(ChessMove move){
+        super(CommandType.MAKE_MOVE);
+        this.move = move;
+    }
 }
