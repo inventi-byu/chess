5b29854711a011978e73795efa8e152af09adf6d
inventi-byu <bcgunn@byu.edu>
2025-04-08 12:08:58 -0600

    Changed MoveCommand to only have move and no username, etc.

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -4,23 +4,11 @@ import chess.ChessGame;
 import chess.ChessMove;
 
 public class MakeMoveCommand extends UserGameCommand {
-    public String username;
-    public String teamColor;
     public ChessMove move;
 
-    public MakeMoveCommand(String username, String teamColor, ChessMove move, String authToken, Integer gameID) {
+    public MakeMoveCommand(String authToken, Integer gameID, ChessMove move) {
         super(CommandType.MAKE_MOVE, authToken, gameID);
-        this.teamColor = teamColor;
         this.move = move;
-        this.username = username;
-    }
-
-    public String getUsername(){
-        return this.username;
-    }
-
-    public String getTeamColor(){
-        return this.teamColor;
     }
 
     public ChessMove getMove(){
